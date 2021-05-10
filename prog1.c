eaa3e702fb4c9197b35c53c1f4616ceac59f761a
chinmay2812 <83934925+chinmay2812@users.noreply.github.com>
2021-05-10 14:25:34 +0530

    Add files via upload

---
+++ prog1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int a,b;
+printf("input the value of num1 and num2:");
+scanf("%d,%d",&a,&b);
+if(a>b)
+printf("num1 is greater\n");
+else
+printf("num2 is greater\n");
+}
